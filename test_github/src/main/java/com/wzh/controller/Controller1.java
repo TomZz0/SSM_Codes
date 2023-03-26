7d6865a831293e3b583aa58507c6a3ce77825ac5
Tomwzh <1210374096@qq.com>
2023-03-26 11:46:56 +0800

    commit message

---
+++ test_github/src/main/java/com/wzh/controller/Controller1.java
@@ -0,0 +1,9 @@
+package com.wzh.controller;
+
+/**
+ * @author wzh
+ * @date 2023年03月26日 11:46
+ * Description:
+ */
+public class Controller1 {
+}
---
+++ test_github/src/main/java/com/wzh/service/Service1.java
@@ -0,0 +1,9 @@
+package com.wzh.service;
+
+/**
+ * @author wzh
+ * @date 2023年03月26日 11:48
+ * Description:
+ */
+public class Service1 {
+}
