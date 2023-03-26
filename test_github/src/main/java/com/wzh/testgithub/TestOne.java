869e290932ecd3bf297c4a19a6a2218cadbb2e02
Tomwzh <1210374096@qq.com>
2023-03-26 11:44:26 +0800

    Test github num 1

---
+++ test_github/src/main/java/com/wzh/testgithub/TestOne.java
@@ -0,0 +1,12 @@
+package com.wzh.testgithub;
+
+/**
+ * @author wzh
+ * @date 2023年03月26日 11:42
+ * Description:
+ */
+public class TestOne {
+    public static void main(String[] args) {
+        System.out.println(666);
+    }
+}
